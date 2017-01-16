335ff93fc2ab9534c8ad14cc9e852bf001ebcdc1
fancl <fanchaolve@sina.cn>
2017-01-16 10:22:00 +0800

    添加功能444

--- app/src/main/java/com/bb/hbx/MyApplication.java
+++ app/src/main/java/com/bb/hbx/MyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.util.DisplayMetrics;
 
+import com.bb.hbx.bean.User;
 import com.bb.hbx.utils.LocationService;
 
 /**
@@ -15,14 +16,22 @@ public class MyApplication extends Application {
 
     public LocationService locationService;
     private static MyApplication INSTANCE = null;
-
     private static Context context;
+
+    public static String latitude;// 纬度
+
+    public static String longitude;//经度
+
+    public static String city;//城市
+
+
+
 
     public static String DUID = "";// 设备唯一码
     public static int widthPixels = 321;// 屏幕宽度
     public static int heightPixels = 481;//屏幕高度
 
-    //public static User user;// 用户信息
+    public static User user;// 用户信息
 
     public static final Context getAppContext() {
         return context;
@@ -50,7 +59,7 @@ public class MyApplication extends Application {
         super.onCreate();
         context = getApplicationContext();
         INSTANCE = this;
-        locationService = new LocationService(context);
+ //       locationService = new LocationService(context);
 //        CrashHandler crashHandler = CrashHandler.getInstance();
 //        crashHandler.init(context);
 
